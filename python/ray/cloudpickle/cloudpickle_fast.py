229db6069e893edbff53194372447872d7eb6ad8
Ben Bell <ben.bell@heronsystems.com>
2020-06-08 19:19:23 -0400

    Make all ray serialized np.arrays non-writeable

--- python/ray/cloudpickle/cloudpickle_fast.py
+++ python/ray/cloudpickle/cloudpickle_fast.py
@@ -445,7 +445,8 @@ def _numpy_frombuffer(buffer, dtype, shape, order):
     array = _frombuffer(buffer, dtype, shape, order)
     # Unfortunately, numpy does not follow the standard, so we still
     # have to set the readonly flag for it here.
-    array.setflags(write=isinstance(buffer, bytearray) or not buffer.readonly)
+    # array.setflags(write=isinstance(buffer, bytearray) or not buffer.readonly)
+    array.setflags(write=False)
     return array
 
 
